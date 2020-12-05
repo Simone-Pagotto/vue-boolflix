8026840878f820f2dc8d046584d92aa5bde5b37b
Simone <simone.pagotto.kg@gmail.com>
2020-12-05 12:30:32 +0100

    Milestone #5
    -aggiunti generi alla scheda

--- js/main.js
+++ js/main.js
@@ -2,25 +2,31 @@ const languageFlags=["en","it","es"];
 const API_KEY = 'aa4673a37382961cbea0f02136d42791';
 
  const myBoolflix = new Vue ({
-     el:'#root',
-     data:{       
-        films:[],
-        imgConfig:'',
-        searchInput:"",
-        isSearchActive: false,
-        languageFlags: [...languageFlags],
-        nStarVote: 5
-        
-     },
-     mounted: function(){
-         axios.get('https://api.themoviedb.org/3/configuration',{
-             params: {
-                 'api_key': API_KEY
-             }
-         }).then(element => this.imgConfig = element.data.images)
-     },
-        
-        
+        el:'#root',
+        data:{       
+           films:[],
+           imgConfig:'',
+           genres:[],
+           searchInput:"",
+           isSearchActive: false,
+           languageFlags: [...languageFlags],
+           nStarVote: 5,
+           nActors: 5
+
+        },
+        mounted: function(){
+            axios.get('https://api.themoviedb.org/3/configuration',{
+                params: {
+                    'api_key': API_KEY
+                }
+            }).then(element => this.imgConfig = element.data.images);
+
+            axios.get('https://api.themoviedb.org/3/genre/movie/list',{
+                params: {
+                    'api_key': API_KEY
+                }
+            }).then(element => this.genres = element.data.genres);
+        },
         methods: {
             filterFilms(){
                 let moviePromise = axios
@@ -30,14 +36,6 @@ const API_KEY = 'aa4673a37382961cbea0f02136d42791';
                             query: this.searchInput,
                         }
                     });
-               /* moviePromise
-                    .then((result) =>{
-                        //azzero l'array dei film: così sostituisco anzichè aggiungere 
-                        //TROVARE UNA POSIZIONE MIGLIORE PER QUEST'OPERAZIONE(MOUNTED??)
-                        this.films.splice(0,this.films.length);
-                        let films = result.data.results;
-                        this.films.push(...films); 
-                    }) */
                 let tvPromise = axios
                     .get("https://api.themoviedb.org/3/search/tv", {
                         params: {
@@ -45,11 +43,6 @@ const API_KEY = 'aa4673a37382961cbea0f02136d42791';
                             query: this.searchInput,
                         }
                     });
-                /* tvPromise
-                    .then((result) =>{
-                        let films = result.data.results;
-                        this.films.push(...films);             
-                    }); */
                 this.films.splice(0, this.films.length);
                 Promise.all([moviePromise, tvPromise])
                         .then((result) => { 
@@ -65,29 +58,11 @@ const API_KEY = 'aa4673a37382961cbea0f02136d42791';
                                             'api_key': API_KEY,
                                         }
                                     }).then(result => {
-                                        this.$set(this.films[i], 'cast', result.data.cast.splice(0, 5))  
+                                        this.$set(this.films[i], 'cast', result.data.cast.splice(0, this.nActors))  
                                     }
                                         )
-                                    /* .catch((error) => console.error(error));*/
-                                /* this.$set(this.obj, 'cast', result.data.cast.splice(0, 5)) */
                             }
                         })  
-                        
-                        
-                //ricerco il cast con un for loop, ricavo tutti gli id
-                /* for (let i = 0; i < this.films.length; i++){
-                    console.log('ciao');
-                    axios
-                        .get(`https://api.themoviedb.org/3/movie/${this.films[i].id}/credits`, {
-                            params: {
-                                'api_key': API_KEY,
-                            }
-                        }).then(result => console.log(result))
-                        .catch((error) => console.error(error)); 
-                            
-                    this.$set(this.obj, 'cast', result.data.cast.splice(0, 5)) 
-                } */
-                    
             },
             voteToStars: function(film){
                 let vote = Math.ceil(film.vote_average / 2);
@@ -99,6 +74,19 @@ const API_KEY = 'aa4673a37382961cbea0f02136d42791';
             showPoster: function(str){ 
                 return `${this.imgConfig.base_url}${this.imgConfig.poster_sizes[3]}${str} ` ; 
             },
+            showGenre: function(arr){
+                let isNotInList = 0;
+                for(let i=0; i<this.genres.length; i++){
+                    if(arr === this.genres[i].id){
+                        return this.genres[i].name;
+                    } else {
+                        isNotInList++;
+                    }
+                }
+                if(isNotInList===this.genres.length){
+                    return 'error';
+                }
+            },
             isFilm: function(film){
                 //Controllo se dentro all'oggetto film ci sono le proprietà caratteristiche di un film o di una serie tv
                 // con il v-if poi reindirizzo ogni singolo oggetto al suo template
@@ -109,17 +97,8 @@ const API_KEY = 'aa4673a37382961cbea0f02136d42791';
             nullImg(e){
                 e.target.src = 'img/error-square.svg';
             },
-            getPunctuation(index) {
-                if(index===4){
-                    return ".";
-                } else {
-                    return ",";
-                }
-            }
+            
             
              
         },
-        computed:{
-            
-        }
  })
